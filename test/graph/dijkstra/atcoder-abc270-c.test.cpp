2d506c17a24362055ece95256e76be3275be4fce
yokota <yokota.yoshiki@rm.is.tohoku.ac.jp>
2024-04-16 03:13:58 +0900

    add test

---
+++ test/graph/dijkstra/atcoder-abc270-c.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://atcoder.jp/contests/abc270/tasks/abc270_c"
+
+#include "../../../graph/dijkstra.cpp"
+
+int main() {
+    ll N, X, Y;
+    cin >> N >> X >> Y;
+
+    Dijkstra tree(N, false);
+    rep(i, N - 1) {
+        ll u, v;
+        cin >> u >> v;
+
+        tree.connect(u - 1, v - 1, 1);
+    }
+
+    tree(X - 1);
+
+    auto ans = tree.path_to(Y - 1);
+    fore(a, ans) ++a;
+
+    cout << ans << endl;
+
+    return 0;
+}
---
+++ test/structure/bit/atcoder-tessoku-book-b59.test.cpp
@@ -0,0 +1,17 @@
+#define IGNORE
+#define PROBLEM "https://atcoder.jp/contests/chokudai_S001/tasks/chokudai_S001_j"
+
+#include "../../../structure/bit.cpp"
+
+int main() {
+    long long N;
+    cin >> N;
+
+    vll A(N);
+    rep(i, N) cin >> A[i];
+
+    BIT tree(N);
+    cout << tree.invension(A) << endl;
+
+    return 0;
+}
