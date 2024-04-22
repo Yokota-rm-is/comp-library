774a33fa248fd110f1b0ebf69eead5cdbd2fc811
yokota <yokota.yoshiki@rm.is.tohoku.ac.jp>
2024-04-23 03:27:35 +0900

    splay-treeのテストを追加

---
+++ test/structure/splay-tree/atcoder-abc350-f.test.cpp
@@ -0,0 +1,68 @@
+#define IGNORE
+#define PROBLEM "https://judge.yosupo.jp/problem/dynamic_sequence_range_affine_range_sum"
+
+#include "../../../structure/splay-tree.cpp"
+
+namespace seg {
+    #include "../../../structure/dualsegmenttree.cpp"
+}
+
+int main() {
+    string S;
+    cin >> S;
+
+    vector<char> T;
+    fore(c, S) T.push_back(c);
+
+    stack<ll> st;
+    vector<pll> A;
+    rep(i, S.size()) {
+        char c = S[i];
+        if (c != '(' and c != ')') {
+
+        }
+        else if (c == '(') st.push(i);
+        else {
+            ll l = st.top();
+            st.pop();
+            ll r = i;
+            A.emplace_back(l, r + 1);
+        }
+    }
+
+    seg::RangeAddPointGet<ll, ll> tree(S.size(), 0);
+    fore(p, A) {
+        auto [l, r] = p;
+        tree.apply(l, r, 1);
+    }
+
+    rep(i, S.size()) {
+        char c = S[i];
+        if (c == '(' or c == ')') continue;
+        if (tree.get(i) % 2 == 0) S[i] = c;
+        else {
+            if (is_upper(c)) S[i] = to_lower(c);
+            else S[i] = to_upper(c);
+        }
+    }
+
+    SplayTreeByIdx<char, ll, NoMapping, NoOperation> splay;
+    rep(i, S.size()) splay.insert_at(i, S[i]);
+
+    fore(p, A) {
+        auto [l, r] = p;
+        splay.reverse(l, r);
+    }
+
+    string ans;
+    rep(i, S.size()) {
+        char c = splay.get(i);
+        if (c == '(' or c == ')') continue;
+
+        ans += c;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+} 
