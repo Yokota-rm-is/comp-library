751d32e599641c08d6fcb7323eef86d7c0861385
yokota <yokota.yoshiki@rm.is.tohoku.ac.jp>
2024-04-24 11:14:27 +0900

    uniteを変更

--- structure/unionfind.cpp
+++ structure/unionfind.cpp
@@ -2,11 +2,11 @@
 #include "../base.cpp"
 
 struct UnionFind {
-    long long V{};
-    vector<long long> par{}; // par[i]: iの親の番号 or サイズ (iが親の時)
+    long long V;
+    vector<long long> par; // par[i]: iの親の番号 or サイズ (iが親の時)
     map<long long, set<long long>> cc;
 
-    explicit UnionFind(long long V) : V(V), par(V, -1) { //最初は全てが根であるとして初期化
+    UnionFind(long long V) : V(V), par(V, -1) { //最初は全てが根であるとして初期化
         rep(i, V) {
             cc[i].insert(i);
         }
@@ -21,23 +21,22 @@ struct UnionFind {
     }
 
     // xとyを連結
-    bool unite(long long x, long long y) {
+    void unite(long long x, long long y) {
         long long rx = find(x); //xの根をrx
         long long ry = find(y); //yの根をry
-        if (rx == ry) return false; //xとyの根が同じ(=同じ木にある)時はそのまま
 
-        // -parはサイズを返す
-        // ryの方がサイズが大きければrxとrxを入れ替える
-        if (-par[rx] < -par[ry]) {
-            swap(rx, ry);
+        if (rx != ry) {
+            // -parはサイズを返す
+            // ryの方がサイズが大きければrxとrxを入れ替える
+            if (-par[rx] < -par[ry]) {
+                swap(rx, ry);
+            }
+
+            par[rx] += par[ry]; // rxのサイズを変更
+            par[ry] = rx; //xとyの根が同じでない(=同じ木にない)時：yの根ryをxの根rxにつける
+            cc[rx].insert(cc[ry].begin(), cc[ry].end());
+            cc.erase(ry);
         }
-
-        par[rx] += par[ry]; // rxのサイズを変更
-        par[ry] = rx; //xとyの根が同じでない(=同じ木にない)時：yの根ryをxの根rxにつける
-        cc[rx].insert(cc[ry].begin(), cc[ry].end());
-        cc.erase(ry);
-
-        return true;
     }
 
     // 2つのデータx, yが属する木が同じならtrueを返す
