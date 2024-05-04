aa58e55755a0f5641955c33ed08755eb0b3f02ea
yokota <yokota.yoshiki@rm.is.tohoku.ac.jp>
2024-05-04 12:06:17 +0900

    テストケースを追加

---
+++ test/structure/lazysegmenttree/aoj-dsl-2-h.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_H&"
+
+#include "../../../structure/lazysegmenttree.cpp"
+
+int main() {
+    ll n, q;
+    cin >> n >> q;
+
+    RangeAddRangeMin<ll, ll> tree(n, 0);
+    while (q--) {
+        ll t;
+        cin >> t;
+
+        if (t == 0) {
+            ll s, t, x;
+            cin >> s >> t >> x;
+            tree.apply(s, t + 1, x);
+        }
+        else {
+            ll s, t;
+            cin >> s >> t;
+            cout << tree.prod(s, t + 1) << endl;
+        }
+    }
+
+    return 0;
+}
