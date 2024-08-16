9b69b39f1eb1ba9f57ae85234eba62d966c9a430
yokota <yokota.yoshiki@rm.is.tohoku.ac.jp>
2024-08-17 04:56:57 +0900

    動的2次元BITを作成

---
+++ test/structure/dynamic-binary-indexed-tree-2d/atcoder-abc339-g.test.cpp
@@ -0,0 +1,42 @@
+#define PROBLEM "https://atcoder.jp/contests/abc339/tasks/abc339_g"
+
+#include "../../../structure/dynamic-binary-indexed-tree-2d.cpp"
+#include "../../../other/compressor.cpp"
+
+int main() {
+    ll N;
+    cin >> N;
+
+    vll A(N);
+    rep(i, N) cin >> A[i];
+
+    Compressor<ll> comp(A);
+
+    DynamicBinaryIndexedTree2D<ll> bit(N, comp.size() + 1);
+    rep(i, N) {
+        bit.add(i, comp[i], A[i]);
+    }
+
+    ll Q;
+    cin >> Q;
+
+    ll B = 0;
+
+    while (Q--) {
+        ll alpha, beta, gamma;
+        cin >> alpha >> beta >> gamma;
+
+        ll L = alpha ^ B;
+        ll R = beta ^ B;
+        ll X = gamma ^ B;
+
+        --L;
+        X = comp.upper_bound(X);
+        
+        ll ans = bit.sum(L, 0, R, X);
+        cout << ans << endl;
+        B = ans;
+    }
+
+    return 0;
+} 
---
+++ test/structure/dynamic-binary-indexed-tree-2d/yosupo-point-add-rectangle-sum.test.cpp
@@ -0,0 +1,76 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_rectangle_sum"
+
+#include "../../../structure/dynamic-binary-indexed-tree-2d.cpp"
+#include "../../../other/compressor.cpp"
+
+int main() {
+    ll N, Q;
+    cin >> N >> Q;
+
+    vector<ll> X, Y, W;
+
+    rep(i, N) {
+        ll x, y, w;
+        cin >> x >> y >> w;
+
+        X.push_back(x);
+        Y.push_back(y);
+        W.push_back(w);
+    }
+
+    vector<vll> query;
+
+    rep(q, Q) {
+        ll t;
+        cin >> t;
+
+        if (t == 0) {
+            ll x, y, w;
+            cin >> x >> y >> w;
+
+            query.push_back({0, x, y, w});
+            X.push_back(x);
+            Y.push_back(y);
+        }
+        else {
+            ll l, d, r, u;
+            cin >> l >> d >> r >> u;
+
+            query.push_back({1, l, d, r, u});
+        }
+    }
+
+    Compressor<ll> comp_x(X), comp_y(Y);
+
+    DynamicBinaryIndexedTree2D<ll> bit(comp_x.size() + 1, comp_y.size() + 1);
+
+    rep(i, N) {
+        ll x = comp_x.find(X[i]);
+        ll y = comp_y.find(Y[i]);
+        ll w = W[i];
+
+        bit.add(x, y, w);
+    }
+
+    rep(q, Q) {
+        ll t = query[q][0];
+
+        if (t == 0) {
+            ll x = comp_x.find(query[q][1]);
+            ll y = comp_y.find(query[q][2]);
+            ll w = query[q][3];
+
+            bit.add(x, y, w);
+        }
+        else {
+            ll l = comp_x.lower_bound(query[q][1]);
+            ll d = comp_y.lower_bound(query[q][2]);
+            ll r = comp_x.lower_bound(query[q][3]);
+            ll u = comp_y.lower_bound(query[q][4]);
+
+            cout << bit.sum(l, d, r, u) << endl;
+        }
+    }
+
+    return 0;
+}
