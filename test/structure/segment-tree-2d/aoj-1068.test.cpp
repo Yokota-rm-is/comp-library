a08414d374832c241596e78175a340c9a4cb0895
yokota <yokota.yoshiki@rm.is.tohoku.ac.jp>
2024-09-28 16:32:23 +0900

    2次元セグメント木を追加

---
+++ test/structure/segment-tree-2d/aoj-1068.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1068"
+
+#include "../../../structure/segment-tree-2d.cpp"
+
+int main() {
+    while (true) {
+        ll r, c, q;
+        cin >> r >> c >> q;
+
+        if (r == 0) break;
+
+        vector<vector<ll>> grid(r, vector<ll>(c, 0));
+        rep(i, r) rep(j, c) cin >> grid[i][j]; 
+
+        SegmentTree2D<ll, ll, Set, Min> tree(grid);
+
+        while (q--) {
+            ll r1, c1, r2, c2;
+            cin >> r1 >> c1 >> r2 >> c2;
+
+            cout << tree.prod(r1, r2 + 1, c1, c2 + 1) << endl;
+        }
+    }
+
+    return 0;
+} 
