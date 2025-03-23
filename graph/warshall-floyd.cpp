51e4cef56d5429791b59e037efd26e448cc0e5c7
yokota <yokota.yoshiki@rm.is.tohoku.ac.jp>
2025-03-23 18:22:46 +0900

    build後のconnect機能を追加

--- graph/warshall-floyd.cpp
+++ graph/warshall-floyd.cpp
@@ -8,11 +8,12 @@
 template<typename T = long long>
 struct WarshallFloyd {
     long long V;
-    vector<vector<T>> cost{};
-    vector<vector<long long>> prev{};
+    vector<vector<T>> cost;
+    vector<vector<long long>> prev;
     bool directed_;
+    bool built = false;
 
-    WarshallFloyd(long long V, bool directed) : V(V), directed_(directed) { //最初は全てが根であるとして初期化
+    WarshallFloyd(long long V, bool directed) : V(V), directed_(directed) {
         cost.assign(V, vector<T>(V, inf64));
         prev.assign(V, vector<long long>(V, -1));
 
@@ -20,24 +21,57 @@ struct WarshallFloyd {
         rep(i, V) rep(j, V) prev[i][j] = i;
     }
 
-    WarshallFloyd(vector<vector<long long>> A) : V(A.size()), cost(A), directed_(true)  {
-        prev.assign(A.size(), vector<long long>(A.size(), -1));
+    WarshallFloyd(vector<vector<long long>> A, bool directed) : V(A.size()), cost(A), directed_(directed) {
+        prev.assign(V, vector<long long>(V, -1));
         rep(i, V) rep(j, V) prev[i][j] = i;
     }
 
     void connect(long long from, long long to, T weight) {
+        if (!built) connect_before_built(from, to, weight);
+        else connect_after_built(from, to, weight);
+    }
+
+    void connect_before_built(long long from, long long to, T weight) {
         assert(0 <= from and from < V);
         assert(0 <= to and to < V);
 
-        cost[from][to] = weight;
-        if (!directed_) cost[to][from] = weight;
+        chmin(cost[from][to], weight);
+        if (!directed_) chmin(cost[to][from], weight);
+    }
+
+    // build後に実行
+    // fromからtoへの辺を追加
+    bool connect_after_built(long long from, long long to, T weight) {
+        assert(0 <= from and from < V);
+        assert(0 <= to and to < V);
+
+        if (cost[from][to] <= weight) return false;
+
+        if (chmin(cost[from][to], weight)) prev[from][to] = to;
+        if (!directed_ and chmin(cost[to][from], weight)) prev[to][from] = from;
+
+        rep(i, V) {
+            rep(j, V) {
+                if (chmin(cost[i][j], cost[i][from] + cost[from][to] + cost[to][j])) {
+                    prev[i][j] = prev[to][j];
+                }
+
+                if (!directed_ and chmin(cost[i][j], cost[i][to] + cost[to][from] + cost[from][j])) {
+                    prev[i][j] = prev[from][j];
+                }
+            }
+        }
+
+        return true;
     }
 
     void operator() () {
-        solve();
+        build();
     }
 
-    void solve() {
+    void build() {
+        built = true;
+
         rep(k, V) {
             rep(i, V) {
                 if (cost[i][k] >= inf64) continue;
