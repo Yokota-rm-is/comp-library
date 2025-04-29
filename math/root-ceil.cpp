3864309b7c84aaa528327dfca0f4b16bf99dc0fe
yokota <yokota.yoshiki@rm.is.tohoku.ac.jp>
2025-04-29 23:14:17 +0900

    unsignedに変更

--- math/root-ceil.cpp
+++ math/root-ceil.cpp
@@ -1,16 +1,16 @@
 #pragma once
 #include "../base.cpp"
 
-long long root_ceil(long long x, long long n) {
+unsigned long long root_ceil(unsigned long long x, unsigned long long n) {
     if (n == 0) return 1;
     if (n == 1) return x;
 
     __int128_t x_ = x;
 
-    long long left = 0, right = ceil(pow((double)INF64, 1. / n)) + 1;
+    unsigned long long left = 0, right = ceil(pow((double)numeric_limits<unsigned long long>::max(), 1. / n)) + 1;
   
     while (right - left > 1) {
-        long long mid = (left + right) / 2;
+        unsigned long long mid = midpoint(left, right);
         __int128_t y = binpow<__int128_t>(mid, n);
         if (y >= x_) right = mid;
         else left = mid;
--- math/root-floor.cpp
+++ math/root-floor.cpp
@@ -1,17 +1,17 @@
 #pragma once
 #include "../base.cpp"
 
-long long root_floor(long long x, long long n) {
+unsigned long long root_floor(unsigned long long x, unsigned long long n) {
     if (n == 0) return 1;
     if (n == 1) return x;
 
-    long long left = 1, right = floor(pow((double)INF64, 1. / n));
+    unsigned long long left = 0, right = floor(pow((double)numeric_limits<unsigned long long>::max(), 1. / n));
     __int128_t x_ = x;
     __int128_t y_ = binpow<__int128_t>(right, n);
     if (y_ <= x_) return right;
   
     while (right - left > 1) {
-        long long mid = midpoint(left, right);
+        unsigned long long mid = midpoint(left, right);
         __int128_t y = binpow<__int128_t>(mid, n);
         if (y <= x_) left = mid;
         else right = mid;
